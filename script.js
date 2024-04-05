08e1538aad2a952e5b8afff49d24d51fba7d91cc
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-05 03:02:41 -0300

    Ajustando el script para que seleccione un numero al azar

---
+++ script.js
@@ -0,0 +1,17 @@
+//aca el juego selecciona un numero al azar para que adivines, con math.floor redondea hacia abajo
+//math.ramdom da el numero al azar, y lo multiplicamos 
+//por la cantidad de numero que queremos que se encuentre
+let numeroAzar = Math.floor(Math.random()*100) + 1;
+
+
+
+
+
+
+
+
+
+// Esta accion se va a ejecutar cuando se presione el boton chequear
+function chequearResultado(){
+
+}
