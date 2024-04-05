48b680ba89fe6ac4ca7329792bd7d566802da3ff
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-05 03:37:57 -0300

    Agregamos contador de intentos, terminamos la funcion para chequear resultados

--- script.js
+++ script.js
@@ -2,24 +2,20 @@
 //math.ramdom da el numero al azar, y lo multiplicamos 
 //por la cantidad de numero que queremos que se encuentre
 let numeroAzar = Math.floor(Math.random()*100) + 1;
-
 let numeroEntrada = document.getElementById('numeroEntrada');
 let mensaje = document.getElementById('mensaje');
-
-
-
-
-
-
-
+let intento = document.getElementById('intento');
+let intentos = 0;
 
 
 // Esta accion se va a ejecutar cuando se presione el boton chequear
 function chequearResultado(){
+    intentos++;
+    intento.textContent = intentos;
     let numeroIngresado = parseInt(numeroEntrada.value);
-
     if(numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)){
         mensaje.textContent = 'Por favor ingresa un numero valido entre 0 y 100';
+        mensaje.style.color = 'black';
         return;
     }
 
@@ -31,6 +27,7 @@ function chequearResultado(){
         mensaje.textContent = '¡¡¡Mas alto!!! ¡¡El numero es mayor al que dijiste!!';
         mensaje.style.color = 'red';
     }else{
-        
+        mensaje.textContent = '¡¡¡Mas bajo!!! ¡¡El numero es menor al que dijiste!!';
+        mensaje.style.color = 'blue';
     }
 }
